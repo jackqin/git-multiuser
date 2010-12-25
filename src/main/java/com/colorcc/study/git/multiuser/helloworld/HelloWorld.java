f288c9b39cf41bcd56b070c8f5f176ffa4427e4a
jackqin <followfollowme@163.com>
2010-12-26 01:58:43 +0800

    Add output
    Change-Id: Ic5714fef6de540e7de402bb448dfa436a08c37d7
    Signed-off-by: jackqin <followfollowme@163.com>

--- src/main/java/com/colorcc/study/git/multiuser/helloworld/HelloWorld.java
+++ src/main/java/com/colorcc/study/git/multiuser/helloworld/HelloWorld.java
@@ -3,11 +3,12 @@ package com.colorcc.study.git.multiuser.helloworld;
 public class HelloWorld {
 
   /**
+   * Add out put by jackqin
    * @param args
    */
   public static void main(String[] args) {
     // TODO Auto-generated method stub
-
+	  System.out.println("Hello World!");
   }
 
 }
